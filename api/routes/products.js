0c4cdd17b30787aa090dd48bf4c34636d197269d
ayangupta9 <ayangupta1000@gmail.com>
2021-09-02 12:45:08 +0530

    RESTful API: setting up MongoDB

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,52 +1,101 @@
 const express = require('express')
+const mongoose = require('mongoose')
 const router = express.Router()
+const Product = require('../models/product')
 
 router.get('/', (req, res, next) => {
-  res.status(200).json({
-    message: 'Handling GET requests to /products'
-  })
+  Product.find()
+    .exec()
+    .then(docs => {
+      if (docs.length > 0) {
+        res.status(200).json(docs)
+      } else {
+        res.status(400).json({
+          error: 'Not Found'
+        })
+      }
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 })
 
 router.post('/', (req, res, next) => {
-  const product = {
-    name: req.body.name,
-    price: req.body.price
-  }
+  const name = req.body.name
+  const price = req.body.price
 
-  res.status(201).json({
-    message: 'Handling POST requests to /products',
-    createdProduct: product
+  const product = new Product({
+    _id: mongoose.Types.ObjectId(),
+    name: name,
+    price: price
   })
+
+  product
+    .save()
+    .then(result => {
+      console.log(result)
+      res.status(201).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
 })
 
 router.get('/:productId', (req, res, next) => {
   const id = req.params.productId
-  if (id === 'special') {
-    res.status(200).json({
-      message: 'You discovered the special ID',
-      id: id
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      if (doc) {
+        res.status(200).json(doc)
+      } else {
+        res.status(404).json({ error: 'Not Found' })
+      }
     })
-  } else {
-    res.status(200).json({
-      message: 'You passed the ID'
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
     })
-  }
 })
-
 
 router.delete('/:productId', (req, res, next) => {
   const id = req.params.productId
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({
+        error: err
+      })
+    })
+
   res.status(200).json({
     message: 'Deleted product'
   })
 })
 
+router.patch('/:productId', (req, res, next) => {
+  const id = req.params.productId
+  const updateOps = {}
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value
+  }
+
+  Product.updateOne({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => {
+      res.status(200).json(result)
+    })
+    .catch(err => {
+      res.status(500).json({ error: err })
+    })
+})
+
 module.exports = router
-
-
-// router.patch('/:productId', (req, res, next) => {
-//   const id = req.params.productId
-//   res.status(200).json({
-//     message: 'Updated product'
-//   })
-// })
--- server.js
+++ server.js
@@ -4,6 +4,25 @@ const app = express()
 const morgan = require('morgan')
 const bodyParser = require('body-parser')
 
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+const uri = process.env.DBURI
+
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+
+const db = mongoose.connection
+
+db.once('open', function () {
+  console.log('Connected Successfully')
+  app.listen(PORT, () => {
+    console.log('Server running on ' + PORT)
+  })
+})
+
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*')
   res.header(
@@ -22,20 +41,20 @@ const productRouter = require('./api/routes/products')
 const orderRouter = require('./api/routes/orders')
 
 app.use(morgan('dev'))
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
 app.use('/products', productRouter)
 app.use('/orders', orderRouter)
 
+// route Not found error middleware
 app.use((req, res, next) => {
   const error = new Error('Not found')
   error.status = 404
   next(error)
 })
 
+// Any other error middleware
 app.use((error, req, res, next) => {
   res.status(error.status || 500)
   res.json({
@@ -43,8 +62,4 @@ app.use((error, req, res, next) => {
       message: error.message
     }
   })
-})
-
-app.listen(PORT, () => {
-  console.log('Server running')
 })
